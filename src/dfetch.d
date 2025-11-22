b11b2e18668734a6f4ce4f90e315e09e90535002
kapijawastaken <gabriellsimic5@gmail.com>
2025-11-22 22:46:23 +0100

    vars

--- src/dfetch.d
+++ src/dfetch.d
@@ -7,11 +7,20 @@ import std.file;
 int main() {
   // vars
   string logos = environment["HOME"] ~ "/.local/share/dfetch/logos/";
-  //string user = environment["USER"] ~ "@" ~ executeShell("hostname").output.strip;
-  //string kernel = executeShell("uname -r").output.strip;
+  string user;
+  if (exists("/etc/hostname")) {
+    user = environment["USER"] ~ "@" ~ readText("/etc/hostname").strip;
+  }
+  else if (exists("/etc/HOSTNAME")) {
+    user = environment["USER"] ~ "@" ~ readText("/etc/HOSTNAME").strip;
+  }
+  else {
+    user = environment["USER"] ~ "@" ~ executeShell("hostname").output.strip;
+  }
+  string kernel = readText("/proc/sys/kernel/osrelease").strip;
   string shell = environment["SHELL"].replaceAll(regex(r"^.*/"), "");
   string distro = readText("/etc/os-release").replaceAll(regex(r"(?m)^(?!PRETTY_NAME=).*"), "").strip.replace("PRETTY_NAME=", "").replace("\"", "");
   // FIXME: rewrite this so no shell cmds are used, theyre slow.
-  writeln(distro);
+  writeln(user);
   return 0;
 }
