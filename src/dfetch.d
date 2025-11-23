6ec8798aff9a2fef6bc623bac678164104a8f07a
kapijawastaken <gabriellsimic5@gmail.com>
2025-11-23 11:34:50 +0100

    fixed merge conflict and stuff

--- src/dfetch.d
+++ src/dfetch.d
@@ -1,8 +1,9 @@
 import std.stdio;
-import std.process;
-import std.string;
-import std.regex;
-import std.file;
+import std.process : executeShell, environment;
+import std.string : strip, replace;
+import std.regex : regex, replaceAll;
+import std.file : exists, readText;
+import std.conv : to;
 
 int main() {
   // vars
@@ -20,7 +21,15 @@ int main() {
   string kernel = readText("/proc/sys/kernel/osrelease").strip;
   string shell = environment["SHELL"].replaceAll(regex(r"^.*/"), "");
   string distro = readText("/etc/os-release").replaceAll(regex(r"(?m)^(?!PRETTY_NAME=).*"), "").strip.replace("PRETTY_NAME=", "").replace("\"", "");
-  // FIXME: rewrite this so no shell cmds are used, theyre slow.
-  writeln(user);
+  
+  // uptime math
+  int seconds = readText("/proc/uptime").replaceAll(regex(r"\..+"), "").strip.to!int;
+  int years = seconds / 31536000;
+  int months = (seconds % 31536000) / 2592000;
+  int weeks = ((seconds % 31536000) % 2592000) / 604800;
+  int days = (seconds % 31536000) / 86400;
+  int hours = (seconds % 86400) / 3600;
+  int minutes = (seconds % 3600) / 60;
+  writeln(seconds);
   return 0;
 }
